7e35d900791ec7ff385cd613d59cb4999c20cb8d
dw1z4rd <toolna@gmail.com>
2026-03-21 16:35:41 -0400

    fix: adjust Footer layout for improved responsiveness and alignment

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="fixed min-w-full bottom-0 m-auto z-50 backdrop-blur-md bg-[--color-surface]/80">
-  <div class="max-w-fit mx-auto px-5 sm:px-8 border-t border-[--color-border] py-4 flex flex-col sm:flex-row items-center justify-between gap-3 text-xs text-[--color-muted]">
-    <span class="font-extrabold text-xl">© {year} Ian Buchanan. All Rights Reserved.</span>
+  <div class="max-w-3xl mx-auto px-5 sm:px-8 border-t border-[--color-border] py-4 flex flex-col sm:flex-row items-center justify-center gap-3 text-xs text-[--color-muted]">
+    <span class="font-extrabold text-lg">© {year} Ian Buchanan. All Rights Reserved.</span>
   </div>
 </footer>
